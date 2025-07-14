06dd35daef2d3db05fcde57f957d1cd6afe20322
rstoltzm-profile <rstoltzm@gmail.com>
2025-07-14 10:36:28 -0700

    adding tests

--- internal/app/server.go
+++ internal/app/server.go
@@ -13,15 +13,21 @@ import (
 )
 
 func Run() error {
+	// load configs
 	cfg := config.LoadConfig()
+
+	// attempt to connect to db
 	conn, err := db.Connect(cfg.DatabaseURL)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
+
+	// keep connection alive until Run() returns (server shutdown)
 	defer conn.Close(context.Background())
 
+	// start the server, pass in conn to NewRouter so api can use it.
 	log.Printf("Starting server on port %s...", cfg.Port)
 	return http.ListenAndServe(":"+cfg.Port, api.NewRouter(conn))
 }
--- internal/db/postgres_test.go
+++ internal/db/postgres_test.go
@@ -1,30 +1,16 @@
 package db
 
 import (
-	"context"
 	"testing"
 
 	"github.com/rstoltzm-profile/video-rental-api/internal/config"
 )
 
-func TestConn(t *testing.T) {
-	cfg := config.LoadConfig()
-	conn, err := Connect(cfg.DatabaseURL)
+func TestEnv(t *testing.T) {
+	got := config.LoadConfig()
 
-	if cfg.DatabaseURL == "" {
-		t.Skip("Skipping test: DATABASE_URL not set")
+	if got.DatabaseURL == "" {
+		t.Errorf("expected DatabaseURL env variable")
 	}
 
-	if err != nil {
-		t.Fatalf("Unable to connect to database: %v", err)
-	}
-	defer conn.Close(context.Background())
-
-	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		t.Fatalf("QueryRow failed: %v", err)
-	}
-
-	t.Logf("DB responded with: %s", greeting)
 }
