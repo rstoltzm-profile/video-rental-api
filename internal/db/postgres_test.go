fd0670e9119dd821c5de1ad472932384b8508486
rstoltzm-profile <rstoltzm@gmail.com>
2025-07-14 10:44:07 -0700

    testing port

--- internal/db/postgres_test.go
+++ internal/db/postgres_test.go
@@ -7,10 +7,22 @@ import (
 )
 
 func TestEnv(t *testing.T) {
-	got := config.LoadConfig()
+	t.Run("test db url", func(t *testing.T) {
+		got := config.LoadConfig()
 
-	if got.DatabaseURL == "" {
-		t.Errorf("expected DatabaseURL env variable")
-	}
+		if got.DatabaseURL == "" {
+			t.Errorf("expected DatabaseURL env variable")
+		}
 
+	})
+
+	t.Run("test db port", func(t *testing.T) {
+		got := config.LoadConfig().Port
+		want := "8080"
+
+		if got != want {
+			t.Errorf("expected port %v, got %v", want, got)
+		}
+
+	})
 }
